0463d33c0b86809874b91d1a8b1129e8e4c5aaf2
SKYLINE2\RobinME <robin.meurisse@skyline.be>
2025-07-15 14:19:21 +0200

    NF: Discrete Option Management added.

---
+++ SLC_SM_IAS_Manage_Discretes/Presenters/DiscreteMgmtAddPresenter.cs
@@ -0,0 +1,56 @@
+﻿namespace SLC_SM_IAS_Manage_Discretes.Presenters
+{
+	using System;
+	using System.Collections.Generic;
+
+	using Skyline.DataMiner.Automation;
+
+	using SLC_SM_Common.API.ConfigurationsApi;
+
+	using SLC_SM_IAS_Manage_Discretes.Views;
+
+	public class DiscreteMgmtAddPresenter
+	{
+		private readonly IEngine engine;
+		private readonly DiscreteMgmtAddView view;
+		private readonly List<Models.DiscreteValue> discreteValues;
+
+		public DiscreteMgmtAddPresenter(IEngine engine, DiscreteMgmtAddView view, List<Models.DiscreteValue> discreteValues)
+		{
+			this.engine = engine;
+			this.view = view;
+			this.discreteValues = discreteValues;
+
+			Validate();
+
+			view.Value.Changed += (sender, args) => Validate(args.Value);
+		}
+
+		public bool Validate()
+		{
+			return Validate(view.Value.Text);
+		}
+
+		public bool Validate(string value)
+		{
+			bool ok = true;
+
+			if (String.IsNullOrEmpty(value))
+			{
+				view.ErrorValue.Text = "Please enter a valid value";
+				ok = false;
+			}
+			else if (discreteValues.Exists(x => x.Value == value))
+			{
+				view.ErrorValue.Text = "Entered value already exists";
+				ok = false;
+			}
+			else
+			{
+				view.ErrorValue.Text = String.Empty;
+			}
+
+			return ok;
+		}
+	}
+}
---
+++ SLC_SM_IAS_Manage_Discretes/Views/DiscreteMgmtDeleteView.cs
@@ -0,0 +1,38 @@
+﻿namespace SLC_SM_IAS_Manage_Discretes.Views
+{
+	using System;
+
+	using Skyline.DataMiner.Automation;
+	using Skyline.DataMiner.Utils.InteractiveAutomationScript;
+
+	using SLC_SM_Common.API.ConfigurationsApi;
+
+	public class DiscreteMgmtDeleteView : Dialog
+	{
+		public DiscreteMgmtDeleteView(IEngine engine) : base(engine)
+		{
+			Title = "Remove Discrete Option";
+
+			int row = 0;
+			AddWidget(LblValue, row, 0);
+			AddWidget(Value, row, 1);
+
+			AddWidget(new WhiteSpace(), ++row, 0);
+			AddWidget(Info, ++row, 0, 1, 2);
+
+			AddWidget(new WhiteSpace(), ++row, 0);
+			AddWidget(BtnClose, ++row, 0);
+			AddWidget(BtnRemove, row, 1);
+		}
+
+		public Label LblValue { get; } = new Label("Discrete Value");
+
+		public DropDown<Models.DiscreteValue> Value { get; } = new DropDown<Models.DiscreteValue> { IsDisplayFilterShown = true };
+
+		public Label Info { get; } = new Label(String.Empty);
+
+		public Button BtnClose { get; } = new Button("Cancel");
+
+		public Button BtnRemove { get; } = new Button("Remove Discrete Option");
+	}
+}
