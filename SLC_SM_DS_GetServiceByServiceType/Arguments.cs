02cdb514294db0c7afdfef73017c486921ba0ef3
Rui Camacho <rui.camacho@skyline.be>
2025-09-17 12:57:32 +0200

    Get Services by Service Type

---
+++ SLC_SM_DS_GetServiceByServiceType/Arguments.cs
@@ -0,0 +1,26 @@
+﻿namespace SLCSMDSGetServiceByServiceType
+{
+	using Skyline.DataMiner.Analytics.GenericInterface;
+
+	internal class Arguments : IGQIInputArguments
+	{
+		private readonly GQIStringArgument serviceTypeArg = new GQIStringArgument("Service Type") { IsRequired = false };
+
+		public string ServiceType { get; set; }
+
+		public GQIArgument[] GetInputArguments()
+		{
+			return new GQIArgument[]
+			{
+				serviceTypeArg,
+			};
+		}
+
+		public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
+		{
+			ServiceType = args.GetArgumentValue(serviceTypeArg);
+
+			return new OnArgumentsProcessedOutputArgs();
+		}
+	}
+}
